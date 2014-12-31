bfbba04e89326a124f2beac1580f8beb4f32f0d5
Jayin Ton <tonjayin@gmail.com>
2014-12-31 21:55:25 +0800

    * update doc & format

--- AlgorithmicAnalysis/实验五/test.py
+++ AlgorithmicAnalysis/实验五/test.py
@@ -3,15 +3,19 @@ __author__ = 'jayin'
 
 import ex5
 
-#-> 4
-ex5.main(4,5,1,1)
+# -> 4
+ex5.main(4, 5, 1, 1)
 
 #-> 7
-ex5.main(4,5,1,2)
+ex5.main(4, 5, 1, 2)
 
 #-> 5
-ex5.main(4,5,2,1)
+ex5.main(4, 5, 2, 1)
 
 #-> 8
-ex5.main(4,5,2,4)
+ex5.main(4, 5, 2, 4)
+
+
+
+
 
